cb57756993d866c9c5e66916a20798ab01a5c32f
dbhadoria <44959398+dbhadoria@users.noreply.github.com>
2023-05-22 16:31:48 -0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,22 +7,94 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    let filtered_users = users.filter((user) => user.username === user1.username);
+    if (filtered_users.length == 0) {
+        return false;
+    }
+    return true
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+        return true;
+    } else {
+        return false;
+    }
 }
-
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+    if (authenticatedUser(username,password)) {
+        let accessToken = jwt.sign({
+        data: password
+        }, 'access', { expiresIn: 60 * 60 * 1000});
+        req.session.authorization = {
+            accessToken,username
+        }
+    return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let book = books[isbn];
+    if (book) {
+        console.log(req.session.authorization.username);
+        let username = req.session.authorization.username;
+        let reviews = book.reviews
+        let review = req.query.review
+        console.log(book);
+        console.log(review);
+        console.log(reviews);
+        console.log(typeof(reviews));
+        if (reviews.length > 0) {
+            const revKeys = Object.keys(reviews);
+            const matchingUsers = [];
+            for (const key of revKeys) {
+                if (key === username) {
+                    reviews.key = review;
+                    matchingUsers.push(book);
+                    return res.status(200).send(`A review with an ISBN ${isbn} has been added/updated`);
+                }
+            }
+            if (matchingUsers.length < 0) {
+                let rev = {
+                    1 : {username, review}
+                }
+                reviews.push(rev);
+                console.log(book)
+                books[isbn].reviews = reviews;
+            } else {
+                for (let i = 0; i < reviews.length; i++) {
+                    if (reviews[i][0] == req.session.username) {
+                        reviews[i][1] = review;
+                        books[isbn].reviews = reviews;
+                        return res.status(200).send(`A review with an ISBN ${isbn} has been added/updated`);
+                    }
+                }
+            }
+        } else {
+            // let rev = {
+            //     1 : {username, review}
+            // }
+            reviews.username = review;
+            books[isbn].reviews = reviews;
+            console.log(books[isbn]);
+        }
+        return res.status(200).send(`A review with an ISBN ${isbn} has been added/updated`);
+    }
+    return res.status(200).send("There was an Error");
 });
 
 module.exports.authenticated = regd_users;
